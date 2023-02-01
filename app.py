ae12bd653970b3e39f4f10902e1d2ba9aef3530e
Manabu Kikuchi <mkikuchi@imprex.co.jp>
2023-02-01 10:05:30 +0900

    updated.

--- app.py
+++ app.py
@@ -60,9 +60,8 @@ jwt.unauthorized_loader(jwt_unauthorized_loader_handler)
 # レスポンスにCORS許可のヘッダーを付与
 @app.after_request
 def after_request(response):
-    # response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST')
     return response
 
 # ログインしてトークンを返却
